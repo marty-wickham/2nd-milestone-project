deeca17ed957a8750bdbe1edf871eda1e27cced2
Ubuntu <ubuntu@ip-172-31-87-93.ec2.internal>
2019-07-18 10:34:42 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/*
+function gameMove() {
+    var x = Math.floor((Math.random()*4) + 1);
+    document.getElementById("random").innerHTML = x;
+}
+
+gameMove();
+*/ // test one
+
---
+++ assets/js/simon.js
@@ -0,0 +1,32 @@
+/* var game = {
+    count: 0,
+    gameMemory: [],
+    playerMemory: [],
+    colors: ["red", "green", "blue", "yellow"]
+};
+
+function newGame() {
+    game.count = 0;
+    game.gameMemory = [];
+    game.playerMemory = [];
+}
+
+newGame();
+
+function gameMove() {
+    game.count++;
+    game.gameMemory.push(game.colors[Math.floor((Math.random() * 4) + 1)]);
+}
+
+gameMove;
+
+function playSequence() {
+    for(var i = 0; i <= game.gameMemory.length; i++) {
+        document.getElementById(game.gameMemory[i]);
+    }
+}
+*/
+
+$("#red").mouseenter(function() {
+    $(this).addClass("highlight");
+})
