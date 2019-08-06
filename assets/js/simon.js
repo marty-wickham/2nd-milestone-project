717974213914818f2388ed1192bb60b8db485b35
Ubuntu <ubuntu@ip-172-31-87-93.ec2.internal>
2019-08-06 20:53:33 +0000

    started on playSequence function

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -1,48 +1,50 @@
 var score = 0;
 var gameSequence = [];
 var playerSequence = [];
+var start = false;
 
 function fillArray() {
     for (var i = 0; i < 30; i++) {
         gameSequence.push(Math.floor((Math.random() * 4) + 1));
     }
+    console.log(gameSequence);
 }
 
 fillArray();
 
 function playRed() {
     $("#red").addClass("highlight-red");
-    
+
     setTimeout(function() {
         $("#red").removeClass("highlight-red")
-        
+
     }, 500);
 }
 
 function playGreen() {
     $("#green").addClass("highlight-green");
-    
+
     setTimeout(function() {
         $("#green").removeClass("highlight-green")
-        
+
     }, 500);
 }
 
 function playBlue() {
     $("#blue").addClass("highlight-blue");
-    
+
     setTimeout(function() {
         $("#blue").removeClass("highlight-blue")
-        
+
     }, 500);
 }
 
 function playYellow() {
     $("#yellow").addClass("highlight-yellow");
-    
+
     setTimeout(function() {
         $("#yellow").removeClass("highlight-yellow")
-        
+
     }, 500);
 }
 
@@ -50,57 +52,58 @@ function playYellow() {
 $(document).ready(function() {
 
     $("#start-btn").click(function() {
-        startGame();
+
+        playSequence();
     });
 
-    $(".col-button").click(function() {
-        
-        button = $(this).attr("id");
-        
-        if (button == "red") {
-            playRed();
-            playerSequence.push(0);
-            console.log(playerSequence);
-        }
-        
-        if (button == "green") {
-            playGreen();
-            playerSequence.push(1);
-            console.log(playerSequence);
-        }
-        
-        if (button == "blue") {
-            playBlue();
-            playerSequence.push(2);
-            console.log(playerSequence);
-        }
-        
-        if (button == "yellow") {
-            playYellow();
-            playerSequence.push(3);
-            console.log(playerSequence);
-        }
-        
-    });
+    if (start == true) {
 
+        $(".col-button").click(function() {
 
+            button = $(this).attr("id");
+
+            if (button == "red") {
+                playRed();
+                playerSequence.push(1);
+                console.log(playerSequence);
+            }
+
+            if (button == "green") {
+                playGreen();
+                playerSequence.push(2);
+                console.log(playerSequence);
+            }
+
+            if (button == "blue") {
+                playBlue();
+                playerSequence.push(3);
+                console.log(playerSequence);
+            }
+
+            if (button == "yellow") {
+                playYellow();
+                playerSequence.push(4);
+                console.log(playerSequence);
+            }
+        });
+    }
 });
 
-function startGame() {
-    playSequence();
-}
-
 function playSequence() {
+
     for (var i = 0; i < score + 1; i++) {
-        if (gameSequence[i] === 0) {
+
+        if (gameSequence[i] === 1) {
             playRed();
+        }
+        if (gameSequence[i] === 2) {
+            playGreen();
+        }
+        if (gameSequence[i] === 3) {
+            playBlue();
+        }
+        if (gameSequence[i] === 4) {
+            playYellow();
         }
     }
 }
-
-
-
-
-
-
-
