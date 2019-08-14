5471b22bd43317c72639beae8e17174e094fd780
Ubuntu <ubuntu@ip-172-31-87-93.ec2.internal>
2019-08-14 19:56:02 +0000

    deleted unecessary files

--- edits.js
+++
@@ -1,9 +0,0 @@
-
-function fillArray() {
-    for (var i = 0; i < 30; i++) {
-        gameSequence.push(Math.floor((Math.random() * 4) + 1));
-    }
-    console.log(gameSequence);
-}
-
-fillArray();
